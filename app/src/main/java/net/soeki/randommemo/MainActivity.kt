bff5efd8adaf2cc32d68fcc753eb25702cf46734
Soeking <great.from.g@gmail.com>
2023-03-07 21:55:33 +0900

    init

---
+++ app/src/main/java/net/soeki/randommemo/MainActivity.kt
@@ -0,0 +1,57 @@
+package net.soeki.randommemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.soeki.randommemo.ui.theme.RandomMemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RandomMemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val list =(1..100).map { "list $it" }
+                    ListScreen(recordList = list)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RandomMemoTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun ListScreen(recordList: List<String>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(recordList) { todo ->
+            Text(todo)
+        }
+    }
+}
